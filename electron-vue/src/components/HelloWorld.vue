678324724e1471ad03581bcb95a98d4cb31a2295
Andrej Oljaca <65299043+aoljaca@users.noreply.github.com>
2021-06-14 14:27:58 -0400

    commit

--- electron-vue/src/components/HelloWorld.vue
+++ electron-vue/src/components/HelloWorld.vue
@@ -5,26 +5,39 @@
       <vue-qrcode value="https://coinmarketcap.com/currencies/burst/" />
       <vue-qrcode value="https://www.coindesk.com/price/cardano" />
       <a class="twitter-timeline" href="https://twitter.com/ethereum?ref_src=twsrc%5Etfw">Tweets by ethereum</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-      <button v-on:click="greet" type="button" class="btn btn-default">Take a Screenshot!</button>
+      <div ref="printMe" style="padding: 10px; background: #f5da55">
+        <h1 style="color: #000; ">Print me!</h1>
+      </div>
+    <!-- OUTPUT -->
+      <img :src="output">
     </div>
 </template>
 <script>
 import VueQrcode from 'vue-qrcode'
-import html2canvas from 'html2canvas';
-import Canvas2Image from 'canvas2image';
 export default {
   components: {
     VueQrcode,
   },
-  methods: {
-    greet: function () {
-      html2canvas(document.querySelector('.specific'), {
-        onrendered: function(canvas) {
-            // document.body.appendChild(canvas);
-          return Canvas2Image.saveAsPNG(canvas);
-        }
-      });
+  data() {
+    return {
+      output: null
     }
+  },
+  methods: {
+    async print() {
+      const el = this.$refs.printMe;
+      // add option type to get the image version
+      // if not provided the promise will return 
+      // the canvas.
+      const options = {
+        type: 'dataURL'
+      }
+      this.output = await this.$html2canvas(el, options);
+    }
+  },
+  mounted() {
+    this.print()
+    alert("hey")
   }
 }
 </script>
