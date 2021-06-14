0575af9bdce6453e498f37925cde2fd8e28da0bb
Andrej Oljaca <65299043+aoljaca@users.noreply.github.com>
2021-06-14 11:55:00 -0400

    commit

--- electron-vue/src/components/HelloWorld.vue
+++ electron-vue/src/components/HelloWorld.vue
@@ -4,6 +4,8 @@
     <vue-qrcode value="https://coinmarketcap.com/currencies/tezos/" />
     <vue-qrcode value="https://coinmarketcap.com/currencies/burst/" />
     <vue-qrcode value="https://www.coindesk.com/price/cardano" />
+    <a class="twitter-timeline" href="https://twitter.com/BBCAfrica?ref_src=twsrc%5Etfw">Tweets by BBCAfrica</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    <a class="twitter-timeline" href="https://twitter.com/ethereum?ref_src=twsrc%5Etfw">Tweets by ethereum</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
   </div>
 </template>
 <script>
